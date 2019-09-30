5f74499e553179c0dfd785cc03f9f22549ffc754
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-30 04:14:08 -0400

    Draws image to screen until user quits out of window

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,125 @@
 #include <iostream>
 #include "SDL.h"
 
-//Screen dimension constants
-const int SCREEN_WIDTH{640};
-const int SCREEN_HEIGHT{480};
+//Screen constants
+const int SCREEN_WIDTH{800};
+const int SCREEN_HEIGHT{600};
 const char *const GAME_TITLE{"BeefRogue v2019.0.1"};
 
-int main(int argc, char *args[]) {
-    //The window we'll be rendering to
-    SDL_Window *window{nullptr};
+// Starts SDL and creates window
+bool init();
 
-    //The surface contained by the window
-    SDL_Surface *screenSurface{nullptr};
+// Loads media
+bool loadMedia();
 
-    //Initialize SDL
+// Frees media and shuts down SDL
+void close();
+
+// The window we'll be rendering to
+SDL_Window *gameWindow = nullptr;
+
+// The surface contained by the window
+SDL_Surface *gameScreenSurface = nullptr;
+
+// The image we will load and show on the screen
+SDL_Surface *gamePlayer = nullptr;
+
+bool init() {
+    // Initialises flag
+    bool success = true;
+
+    // Initialises SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
     } else {
-        //Create window
-        window = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == nullptr) {
+        // Create window
+        gameWindow = SDL_CreateWindow(GAME_TITLE, 300, 300,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gameWindow == nullptr) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
         } else {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            //Fill the surface white
-            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Wait four seconds
-            SDL_Delay(4000);
+            // Get window surface
+            gameScreenSurface = SDL_GetWindowSurface(gameWindow);
         }
     }
 
-    //Destroy window
-    SDL_DestroyWindow(window);
+    return success;
+}
 
-    //Quit SDL subsystems
+bool loadMedia()
+{
+    // loading success flag
+    bool success = true;
+
+    // load splash image
+    gamePlayer = SDL_LoadBMP("../sprites/player.bmp");
+    if (gamePlayer == nullptr)
+    {
+        printf("Unable to load image %s! SDL_Error: %s\n", "../sprites/player.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    // Deallocate surface
+    SDL_FreeSurface(gamePlayer);
+    gamePlayer = nullptr;
+
+    // Destroy window
+    SDL_DestroyWindow(gameWindow);
+    gameWindow = nullptr;
+
+    // Quit SDL subsystems
     SDL_Quit();
+}
+
+int main(int argc, char *args[]) {
+    // Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        // Load media
+        if (!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+    }
+
+    // Main loop flag
+    bool quit = false;
+
+    // Event handler
+    SDL_Event event;
+
+    // Game loop
+    while (!quit)
+    {
+        // Handle events on queue
+        while (SDL_PollEvent(&event) != 0)
+        {
+            // User requests quit
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Apply the image
+        SDL_BlitSurface(gamePlayer, nullptr, gameScreenSurface, nullptr);
+
+        // Update the surface
+        SDL_UpdateWindowSurface(gameWindow);
+    }
+
+    // Free resources and close SDL
+    close();
 
     return 0;
 }
