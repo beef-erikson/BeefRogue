5858f75f8e46e921cece12bbe9d3f1fe2dd6a1ec
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-30 23:21:36 -0400

    Added up/down/left/right commands and player sprites for them. Currently doesn't move.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@ const int SCREEN_WIDTH{800};
 const int SCREEN_HEIGHT{600};
 const char *const GAME_TITLE{"BeefRogue v2019.0.1"};
 
+// Inputs
+enum KeyPress {
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
 // Starts SDL and creates window
 bool init();
 
@@ -15,14 +25,20 @@ bool loadMedia();
 // Frees media and shuts down SDL
 void close();
 
+// Loads individual image
+SDL_Surface *loadSurface(const std::string& path);
+
 // The window we'll be rendering to
 SDL_Window *gameWindow = nullptr;
 
 // The surface contained by the window
 SDL_Surface *gameScreenSurface = nullptr;
 
-// The image we will load and show on the screen
-SDL_Surface *gamePlayer = nullptr;
+// The images that correspond to a keypress
+SDL_Surface *gameKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+
+// Current displayed image
+SDL_Surface *gameCurrentSurface = nullptr;
 
 bool init() {
     // Initialises flag
@@ -48,27 +64,55 @@ bool init() {
     return success;
 }
 
-bool loadMedia()
-{
+bool loadMedia() {
     // loading success flag
     bool success = true;
 
-    // load splash image
-    gamePlayer = SDL_LoadBMP("../sprites/player.bmp");
-    if (gamePlayer == nullptr)
+    // load default surface
+    gameKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("../sprites/playerDown.bmp");
+    if (gameKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == nullptr) {
+        printf("Unable to load default image! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    // load up surface
+    gameKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("../sprites/playerUp.bmp");
+    if (gameKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == nullptr)
     {
-        printf("Unable to load image %s! SDL_Error: %s\n", "../sprites/player.bmp", SDL_GetError());
+        printf("Unable to load up image! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    // load down surface
+    gameKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("../sprites/playerDown.bmp");
+    if (gameKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == nullptr) {
+        printf("Unable to load down image! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    // load left surface
+    gameKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("../sprites/playerLeft.bmp");
+    if (gameKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == nullptr)
+    {
+        printf("Unable to load left image! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    // load right surface
+    gameKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("../sprites/playerRight.bmp");
+    if (gameKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == nullptr)
+    {
+        printf("Unable to load right image! SDL_Error: %s\n", SDL_GetError());
         success = false;
     }
 
     return success;
 }
 
-void close()
-{
+void close() {
     // Deallocate surface
-    SDL_FreeSurface(gamePlayer);
-    gamePlayer = nullptr;
+    SDL_FreeSurface(gameCurrentSurface);
+    gameCurrentSurface = nullptr;
 
     // Destroy window
     SDL_DestroyWindow(gameWindow);
@@ -78,17 +122,25 @@ void close()
     SDL_Quit();
 }
 
+SDL_Surface *loadSurface(const std::string& path)
+{
+    // Load image at specified path
+    SDL_Surface *loadedSurface = SDL_LoadBMP(path.c_str());
+    if (loadedSurface == nullptr)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    }
+
+    return loadedSurface;
+}
+
 int main(int argc, char *args[]) {
     // Start up SDL and create window
-    if (!init())
-    {
+    if (!init()) {
         printf("Failed to initialize!\n");
-    }
-    else
-    {
+    } else {
         // Load media
-        if (!loadMedia())
-        {
+        if (!loadMedia()) {
             printf("Failed to load media!\n");
         }
     }
@@ -99,20 +151,48 @@ int main(int argc, char *args[]) {
     // Event handler
     SDL_Event event;
 
+    // Set default surface to display
+    gameCurrentSurface = gameKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
     // Game loop
-    while (!quit)
-    {
+    while (!quit) {
         // Handle events on queue
-        while (SDL_PollEvent(&event) != 0)
-        {
+        while (SDL_PollEvent(&event) != 0) {
             // User requests quit
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
+            // User presses a key
+            else if (event.type == SDL_KEYDOWN)
+            {
+                // Select surfaces based on key press
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_UP:
+                    case SDLK_w:
+                        gameCurrentSurface = gameKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+                        break;
+
+                    case SDLK_DOWN:
+                    case SDLK_s:
+                        gameCurrentSurface = gameKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+                        break;
+
+                    case SDLK_LEFT:
+                    case SDLK_a:
+                        gameCurrentSurface = gameKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+                        break;
+
+                    case SDLK_RIGHT:
+                    case SDLK_d:
+                        gameCurrentSurface = gameKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+                        break;
+                }
+            }
         }
 
         // Apply the image
-        SDL_BlitSurface(gamePlayer, nullptr, gameScreenSurface, nullptr);
+        SDL_BlitSurface(gameCurrentSurface, nullptr, gameScreenSurface, nullptr);
 
         // Update the surface
         SDL_UpdateWindowSurface(gameWindow);
